d69cbb6b9c9770206befd1a26d317562575ad8b7
dmalitesta <d.malitesta@gmail.com>
2024-02-05 08:44:05 +0100

    Ducho 2.0 docker.

--- run_split.py
+++ run_split.py
@@ -1,5 +1,11 @@
-from elliot.run import run_experiment
 import os
+import shutil
+
+from elliot.run import run_experiment
 
 if not (os.path.exists('./data/baby/train.tsv') and os.path.exists('./data/baby/val.tsv') and os.path.exists('./data/baby/test.tsv')):
     run_experiment(f"config_files/run_split.yml")
+    shutil.move('./data/baby_splits/test.tsv', './data/baby/test.tsv')
+    shutil.move('./data/baby_splits/0/0/train.tsv', './data/baby/train.tsv')
+    shutil.move('./data/baby_splits/0/0/val.tsv', './data/baby/val.tsv')
+    shutil.rmtree('./data/baby_splits/')
