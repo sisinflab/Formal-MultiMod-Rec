94770f70b2a23ea9333108c601a22c51823b22fa
dmalitesta <d.malitesta@gmail.com>
2024-02-05 13:38:23 +0100

    Ducho 2.0 colab.

--- run_split.py
+++ run_split.py
@@ -4,7 +4,7 @@ import shutil
 from elliot.run import run_experiment
 
 if not (os.path.exists('./data/baby/train.tsv') and os.path.exists('./data/baby/val.tsv') and os.path.exists('./data/baby/test.tsv')):
-    run_experiment(f"config_files/run_split.yml")
+    run_experiment(f"config_files/split.yml")
     shutil.move('./data/baby_splits/test.tsv', './data/baby/test.tsv')
     shutil.move('./data/baby_splits/0/0/train.tsv', './data/baby/train.tsv')
     shutil.move('./data/baby_splits/0/0/val.tsv', './data/baby/val.tsv')
