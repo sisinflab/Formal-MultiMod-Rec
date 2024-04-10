ccb4e843888e92c5d12632d916a28cd16846dabd
Daniele Malitesta <57352389+danielemalitesta@users.noreply.github.com>
2024-04-10 16:24:37 +0200

    Update run_split.py

--- run_split.py
+++ run_split.py
@@ -3,9 +3,15 @@ import shutil
 
 from elliot.run import run_experiment
 
-if not (os.path.exists('./data/baby/train.tsv') and os.path.exists('./data/baby/val.tsv') and os.path.exists('./data/baby/test.tsv')):
+import argparse
+
+parser = argparse.ArgumentParser(description="Run training and evaluation.")
+parser.add_argument('--data', type=str, default='baby')
+args = parser.parse_args()
+
+if not (os.path.exists(f'./data/{args.data}/train.tsv') and os.path.exists(f'./data/{args.data}/val.tsv') and os.path.exists(f'./data/{args.data}/test.tsv')):
     run_experiment(f"config_files/split.yml")
-    shutil.move('./data/baby_splits/0/test.tsv', './data/baby/test.tsv')
-    shutil.move('./data/baby_splits/0/0/train.tsv', './data/baby/train.tsv')
-    shutil.move('./data/baby_splits/0/0/val.tsv', './data/baby/val.tsv')
-    shutil.rmtree('./data/baby_splits/')
+    shutil.move(f'./data/{args.data}_splits/0/test.tsv', f'./data/{args.data}/test.tsv')
+    shutil.move(f'./data/{args.data}_splits/0/0/train.tsv', f'./data/{args.data}/train.tsv')
+    shutil.move(f'./data/{args.data}_splits/0/0/val.tsv', f'./data/{args.data}/val.tsv')
+    shutil.rmtree(f'./data/{args.data}_splits/')
